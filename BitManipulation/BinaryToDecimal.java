2d042062dfcba36948867b669d9d26c38fcfa55c
Aastha <aasthabisht2345@gmail.com>
2023-08-30 20:33:04 +0530

    directly convert from binary to decimal

---
+++ BitManipulation/BinaryToDecimal.java
@@ -0,0 +1,9 @@
+package BitManipulation;
+
+// Convert directly from binary to decimal
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        int number = 0b11101001; // Use 0b_ and add the binary value u want to convert in _
+        System.out.println(number);
+    }
+}
