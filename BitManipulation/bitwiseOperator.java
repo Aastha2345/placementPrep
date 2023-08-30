5c8f5bfdc11f3f5970cc052909c274e1e5dc1dc7
Aastha <aasthabisht2345@gmail.com>
2023-08-30 16:56:51 +0530

    bitwise operator added

---
+++ BitManipulation/bitwiseOperator.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class bitwiseOperator{
+    public static void main(String[] args) {
+        int a = 17, b=5;
+        System.out.println("a & b = "+ (a&b));
+        System.out.println("a | b = " + (a | b));
+        System.out.println("~ a = "+ (~a));
+        System.out.println("a ^ b = "+ (a ^ b));
+
+        // left shift and right shift oprators
+        int c=10;
+        System.out.println("c << 2 = " + (c<<2)); // left shift
+        System.out.println("c >> 2 = " + (c>>2)); // right shift
+    }
+}
